d87e347fc82d34f142ded963a8ed6aceea8f8c11
riiach <riiachoii@gmail.com>
2026-05-12 15:33:52 +0900

    fix: prisma import fix

--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@/app/generated/prisma/client.ts';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
